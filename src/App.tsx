74314114b75f85c8e9bbe750260c167a40b39d39
c0mplex <haohoang30092003@gmail.com>
2025-10-29 21:52:28 +0700

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+// Contexts
+import { AuthProvider } from './contexts/AuthContext'
+// General Layouts
+import UserLayout from './layouts/UserLayout'
+import Home from './pages/General/Home'
+import About from './pages/General/About'
+import Contact from './pages/General/Contact'
+import AdminLayout from './layouts/AdminLayout'
+import AdminDashboard from './pages/Admin/AdminDashboard'
+import MedicineManagement from './pages/Admin/MedicineManagement'
+import ServiceManagement from './pages/Admin/ServiceManagement'
 import Chat from './components/chat'
-
 function App() {
+
   return (
-    <Chat />
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<UserLayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='chat' element={<Chat />} />
+        </Route>
+
+        <Route path='admin' element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path='medicines' element={<MedicineManagement />} />
+          <Route path='services' element={<ServiceManagement />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   )
 }
 
